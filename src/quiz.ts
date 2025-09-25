8c67eae4a325aad4986f405f44228d6bf177e531
simpolism <simpolism@gmail.com>
2025-09-25 11:30:50 -0400

    Copy fix

--- src/quiz.ts
+++ src/quiz.ts
@@ -28,7 +28,7 @@ interface QuizElements {
 const template = `
   <header>
     <h1>Is It Conscious?</h1>
-    <p>A quiz to figure out where you stand in the consciousness debate.</p>
+    <p>Figure out where you stand in the consciousness debate.</p>
   </header>
 
   <div class="container">
